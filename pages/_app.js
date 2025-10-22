7bcbd741bbf0f753ce5d0024272a54cab1f43c4b
Mohnish27-dev <mohnishpamnani08@gmail.com>
2025-10-22 17:19:18 +0530

    Add SVG favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { SessionProvider } from "next-auth/react"
+import Head from 'next/head'
 import '../styles/globals.css'
 
 
@@ -6,8 +7,28 @@ export default function app({
   Component, pageProps: { session, ...pageProps }
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps}/>
-    </SessionProvider>
+
+    <>
+      <Head>
+        <title>Indigenous Cultures RAG Chatbot</title>
+
+        {/* Basic favicon (recommended to include a favicon.ico in /public) */}
+        <link rel="icon" href="/favicon.svg" />
+
+        {/* PNG favicons for better support & sizes */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        {/* Apple touch icon for iOS home-screen */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* Optional cache-busting parameter if you need to force browsers to update */}
+        {/* <link rel="icon" href="/favicon.ico?v=2" /> */}
+      </Head>
+      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
